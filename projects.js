043bffd1881b53d172ae42e95ecdc2bb10ef2f70
Emmynice422 <aemmykid@gmail.com>
2024-08-07 00:51:15 +0100

    First Commit

---
+++ projects.js
@@ -0,0 +1,123 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".far");
+const colorsArray = ["red", "orange", "lightblue", "lightgreen", "green"];
+
+updateRating(0);
+
+starsEl.forEach((starEl, index) => {
+  starEl.addEventListener("click", () => {
+    updateRating(index);
+  });
+});
+
+function updateRating(index) {
+  starsEl.forEach((starEl, idx) => {
+    if (idx < index + 1) {
+      starEl.classList.add("active");
+    } else {
+      starEl.classList.remove("active");
+    }
+  });
+
+  emojisEl.forEach((emojiEl) => {
+    emojiEl.style.transform = `translateX(-${index * 50}px)`;
+    emojiEl.style.color = colorsArray[index];
+  });
+}
+const timerEl = document.getElementById("timer");
+const startButtonEl = document.getElementById("start");
+const stopButtonEl = document.getElementById("stop");
+const resetButtonEl = document.getElementById("reset");
+
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
+
+function startTimer() {
+  startTime = Date.now() - elapsedTime;
+
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    timerEl.textContent = formatTime(elapsedTime);
+  }, 10);
+
+  startButtonEl.disabled = true;
+  stopButtonEl.disabled = false;
+}
+
+function formatTime(elapsedTime) {
+  const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+  const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  return (
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+    "." +
+    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+  );
+}
+function stopTimer() {
+  clearInterval(timerInterval);
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
+}
+function resetTimer() {
+  clearInterval(timerInterval);
+
+  elapsedTime = 0;
+  timerEl.textContent = "00:00:00";
+
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
+}
+
+startButtonEl.addEventListener("click", startTimer);
+stopButtonEl.addEventListener("click", stopTimer);
+resetButtonEl.addEventListener("click", resetTimer);
+
+const testimonials = [
+    {
+      name: "Cherise G",
+      photoUrl:
+        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+      text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
+    },
+    {
+      name: "Rosetta Q",
+      photoUrl:
+        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+      text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
+    },
+    {
+      name: "Constantine V",
+      photoUrl:
+        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+      text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
+    },
+  ];
+  
+  const imgEl = document.querySelector("img");
+  const textEl = document.querySelector(".text");
+  const usernameEl = document.querySelector(".username");
+  
+  let idx = 0;
+  
+  updateTestimonial();
+  
+  function updateTestimonial() {
+    const { name, photoUrl, text } = testimonials[idx];
+    imgEl.src = photoUrl;
+    textEl.innerText = text;
+    usernameEl.innerText = name;
+    idx++;
+    if (idx === testimonials.length) {
+      idx = 0;
+    }
+    setTimeout(() => {
+      updateTestimonial();
+    }, 10000);
+  }
